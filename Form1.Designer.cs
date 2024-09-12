c126dfc92c43d4e5a6dd6497c34db6e367a51aa9
memo27 <gjvegav@uamv.edu.ni>
2024-09-12 15:40:45 -0600

    Completed PracticaControlFlujo exercise

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Evaluacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbNombre = new System.Windows.Forms.Label();
+            this.lbApellido = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbApellido = new System.Windows.Forms.TextBox();
+            this.btAgregar = new System.Windows.Forms.Button();
+            this.lbPersonas = new System.Windows.Forms.ListBox();
+            this.lbDateTime = new System.Windows.Forms.Label();
+            this.dtFecha = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // lbNombre
+            // 
+            this.lbNombre.AutoSize = true;
+            this.lbNombre.Location = new System.Drawing.Point(112, 66);
+            this.lbNombre.Name = "lbNombre";
+            this.lbNombre.Size = new System.Drawing.Size(56, 16);
+            this.lbNombre.TabIndex = 0;
+            this.lbNombre.Text = "Nombre";
+            // 
+            // lbApellido
+            // 
+            this.lbApellido.AutoSize = true;
+            this.lbApellido.Location = new System.Drawing.Point(112, 119);
+            this.lbApellido.Name = "lbApellido";
+            this.lbApellido.Size = new System.Drawing.Size(57, 16);
+            this.lbApellido.TabIndex = 1;
+            this.lbApellido.Text = "Apellido";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(208, 66);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(209, 22);
+            this.tbNombre.TabIndex = 2;
+            // 
+            // tbApellido
+            // 
+            this.tbApellido.Location = new System.Drawing.Point(208, 113);
+            this.tbApellido.Name = "tbApellido";
+            this.tbApellido.Size = new System.Drawing.Size(209, 22);
+            this.tbApellido.TabIndex = 3;
+            // 
+            // btAgregar
+            // 
+            this.btAgregar.Location = new System.Drawing.Point(447, 84);
+            this.btAgregar.Name = "btAgregar";
+            this.btAgregar.Size = new System.Drawing.Size(137, 81);
+            this.btAgregar.TabIndex = 4;
+            this.btAgregar.Text = "Ingresar";
+            this.btAgregar.UseVisualStyleBackColor = true;
+            this.btAgregar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbPersonas
+            // 
+            this.lbPersonas.FormattingEnabled = true;
+            this.lbPersonas.ItemHeight = 16;
+            this.lbPersonas.Location = new System.Drawing.Point(208, 243);
+            this.lbPersonas.Name = "lbPersonas";
+            this.lbPersonas.Size = new System.Drawing.Size(270, 132);
+            this.lbPersonas.TabIndex = 5;
+            // 
+            // lbDateTime
+            // 
+            this.lbDateTime.AutoSize = true;
+            this.lbDateTime.Location = new System.Drawing.Point(115, 169);
+            this.lbDateTime.Name = "lbDateTime";
+            this.lbDateTime.Size = new System.Drawing.Size(45, 16);
+            this.lbDateTime.TabIndex = 6;
+            this.lbDateTime.Text = "Fecha";
+            // 
+            // dtFecha
+            // 
+            this.dtFecha.Location = new System.Drawing.Point(208, 164);
+            this.dtFecha.Name = "dtFecha";
+            this.dtFecha.Size = new System.Drawing.Size(200, 22);
+            this.dtFecha.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dtFecha);
+            this.Controls.Add(this.lbDateTime);
+            this.Controls.Add(this.lbPersonas);
+            this.Controls.Add(this.btAgregar);
+            this.Controls.Add(this.tbApellido);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.lbApellido);
+            this.Controls.Add(this.lbNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbNombre;
+        private System.Windows.Forms.Label lbApellido;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbApellido;
+        private System.Windows.Forms.Button btAgregar;
+        private System.Windows.Forms.ListBox lbPersonas;
+        private System.Windows.Forms.Label lbDateTime;
+        private System.Windows.Forms.DateTimePicker dtFecha;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Evaluacion
+{
+    public partial class Form1 : Form
+    {
+        Personas[] per = new Personas[20];
+        int index = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nombre, apellido;
+            DateTime nacimiento;
+            int edad;
+            if(string.IsNullOrEmpty(tbApellido.Text) || string.IsNullOrEmpty(tbNombre.Text))
+            {
+
+            }
+            else
+            {
+                nombre= tbNombre.Text;
+                apellido= tbApellido.Text;
+                nacimiento = dtFecha.Value;
+                edad= caledad(nacimiento);
+
+
+                per[index]= new Personas(nombre, apellido, edad);
+                
+                index++;
+
+                agregar();
+                
+            }
+        }
+        void agregar()
+        {
+            lbPersonas.Items.Clear();
+            for (int i = 0; i < index; i++)
+            {
+                lbPersonas.Items.Add(per[i].nombre + " " + per[i].apellido + " " + per[i].edad);
+                
+            }
+        }
+
+        public int caledad(DateTime fecha)
+        {
+            DateTime hoy = DateTime.Today;
+            return hoy.Year - fecha.Year;
+        }
+    }
+}
